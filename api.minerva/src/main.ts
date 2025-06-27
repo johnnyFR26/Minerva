ae74010ab75d64dd6f3cf04a8ea58ae7f0f5ea5e
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-06-27 00:19:25 -0300

    export server to vercel

--- api.minerva/src/main.ts
+++ api.minerva/src/main.ts
@@ -21,3 +21,10 @@ server.listen({ port, host }, (err) => {
     console.log(`[ ready ] http://${host}:${port}`);
   }
 });
+
+export default async (req: any, res: any) => {
+  await server.ready();
+  server.server.emit("request", req, res);
+};
+
+export { server };
