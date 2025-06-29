2ed3772ad469915728f80e61c3f1524ba0e9e4a6
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-06-29 11:38:20 -0300

    fixes prisma

--- api.minerva/src/main.ts
+++ api.minerva/src/main.ts
@@ -1,30 +1,29 @@
 import Fastify from 'fastify';
 import { app } from './app/app';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-// Instantiate Fastify with some config
 const server = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
 server.register(app);
 
-// Start listening.
-server.listen({ port, host }, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
-  }
-});
+if (require.main === module) {
+  const host = process.env.HOST ?? 'localhost';
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+  server.listen({ port, host }, (err) => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    } else {
+      console.log(`[ ready ] http://${host}:${port}`);
+    }
+  });
+}
 
 export default async (req: any, res: any) => {
   await server.ready();
-  server.server.emit("request", req, res);
+  server.server.emit('request', req, res);
 };
 
 export { server };
