0a3b5b9895f03bf6ec46995ed191cff145325349
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-01 11:59:41 -0300

    fixes autoload

--- api.minerva/src/app/app.ts
+++ api.minerva/src/app/app.ts
@@ -11,17 +11,14 @@ export async function app(fastify: FastifyInstance, opts: AppOptions) {
   // Do not touch the following lines
 
   // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
+    dir: path.join(__dirname, 'src/app/plugins'),
     options: { ...opts },
   });
 
   // This loads all plugins defined in routes
-  // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
+    dir: path.join(__dirname, 'src/app/routes'),
     options: { ...opts },
   });
 }
