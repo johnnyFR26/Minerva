040ba773a681b9c5a6e20924f8774130a80f3f19
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-02 10:46:47 -0300

    refact main

--- api.minerva/src/main.ts
+++ api.minerva/src/main.ts
@@ -8,23 +8,27 @@ const server = Fastify({
 
 registerRoutes(server);
 
-if (require.main === module) {
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-  server.listen({ port, host }, (err) => {
+server.get("/", async (request, reply) => {
+  reply.send("Server running");
+});
+
+async function bootstrap() {
+
+  server.listen({ port: 3000 }, (err, address) => {
     if (err) {
-      server.log.error(err);
+      console.error(err);
       process.exit(1);
-    } else {
-      console.log(`[ ready ] http://${host}:${port}`);
     }
+    console.log(`Server listening at ${address}`);
   });
 }
 
+bootstrap();
+
 export default async (req: any, res: any) => {
   await server.ready();
-  server.server.emit('request', req, res);
+  server.server.emit("request", req, res);
 };
 
 export { server };
