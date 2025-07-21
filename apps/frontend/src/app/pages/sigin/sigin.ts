6af408ae9c2858862b5311b26df1efb8b37661c0
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-21 12:42:41 -0300

    feat: adiciona angular material

---
+++ apps/frontend/src/app/pages/sigin/sigin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sigin',
+  imports: [],
+  templateUrl: './sigin.html',
+  styleUrl: './sigin.scss'
+})
+export class Sigin {
+
+}
