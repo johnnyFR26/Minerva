b5cdcb025f53bb91bb6f85341d6d90128ed28b81
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-21 09:20:24 -0300

    feat: user routes

--- apps/api/v1/src/routes/user.routes.ts
+++ apps/api/v1/src/routes/user.routes.ts
@@ -5,4 +5,8 @@ const controller = new UserController()
 
 export default async function UserRoutes(server: FastifyInstance) {
     server.get("/users", controller.getAll)
+    server.get("/users/:id", controller.get)
+    server.post("/users", controller.create)
+    server.patch("/users/:id", controller.patch)
+    server.delete("/users/:id", controller.delete)
 }
