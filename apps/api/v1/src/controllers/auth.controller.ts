a22a4c5b96948a3c06f721dae80cf21424c2166e
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-21 09:22:12 -0300

    feat: auth controller

---
+++ apps/api/v1/src/controllers/auth.controller.ts
@@ -0,0 +1,46 @@
+import { bcrypt } from 'bcrypt';
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { db } from "../lib/db";
+import jwt from "jsonwebtoken"
+
+export class AuthController {
+    async login(request: FastifyRequest<{Body: {email: string, password: string}}>, response: FastifyReply) {
+
+        const validate = z.object({
+            email: z.string().email(),
+            password: z.string()
+        }).safeParse(request.body)
+
+        if (!validate.success) {
+            return response.status(400).send(validate.error)
+        }
+
+        const { email, password } = validate.data
+
+        const user = await db.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if (!user) {
+            return response.status(400).send({
+                message: "User not found"
+            })
+        }
+
+        if (!await bcrypt.compare(password, user.password)) {
+            return response.status(400).send({
+                message: "Invalid password"
+            })
+        }
+
+        const token = await jwt.sign({email: user.email}, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+        return response.status(200).send({
+            token,
+            user
+        })
+    }
+}
