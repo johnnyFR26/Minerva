39b6e6d4f1dfeb8d616b6c87cd17a83e557723d5
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-22 09:25:27 -0300

    feat: main routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then((m) => m.Dashboard),
+        canActivate: [authGuard]
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login').then((m) => m.Login)
+    },
+    {
+        path: 'sigin',
+        loadComponent: () => import('./pages/sigin/sigin').then((m) => m.Sigin)
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+];
